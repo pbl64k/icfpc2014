599efa1828a16a41df658f17f16ea9b3098e780b
Pavel Lepin <pbl64k@gmail.com>
2014-07-25 22:42:23 +0300

    The beginnings of an actual codegen.

--- code/scimitar.hs
+++ code/scimitar.hs
@@ -1,11 +1,17 @@
+import Data.List
+import qualified Data.Map as M
+import Data.Maybe
 import Text.ParserCombinators.ReadP
 
 data Ast = Lit Int | Var String | Lam [String] Ast | Let [(String, Ast)] Ast | App [Ast] | Spec String [Ast] deriving (Show, Eq, Ord)
-data Program = Prog [(String, Ast)] (Maybe Ast) deriving (Show, Eq, Ord)
+data Program = Prog [(String, Ast)] Ast deriving (Show, Eq, Ord)
+
+data Arg = Num Int | Lbl String deriving (Show, Eq, Ord)
+data Op = Cmt String | Label String | Op String [Arg] deriving (Show, Eq, Ord)
 
 keyws = ["def", "fun", "let"]
 
-spec = ["+", "-", "*", "/", "=", "if", "do"]
+spec = ["+", "-", "*", "/", "=", ">", "<", ">=", "<=", "atom?", "cons", "car", "cdr", "if", "do"]
 
 reserved = (`elem` (keyws ++ spec))
 
@@ -29,7 +35,7 @@ p_alspec = choice [p_alpha, p_spec]
 
 p_idchar = choice [p_alnum, p_spec]
 
-p_bw o c = (char o) `between` (char c)
+p_bw o c = (char o >> p_ows) `between` (p_ows >> char c)
 
 p_bwpar = p_bw '(' ')'
 
@@ -68,13 +74,12 @@ p_lam = p_par $ do
     args <- p_par (p_id `sepBy` p_ws)
     p_ws
     expr <- p_expr
-    p_ows
     return (Lam args expr)
 
 p_let = p_par $ do
     string "let"
     p_ws
-    vars <- p_par (between p_ows p_ows (p_varp `sepBy1` p_ws))
+    vars <- p_par (p_varp `sepBy1` p_ws)
     p_ws
     expr <- p_expr
     p_ows
@@ -107,13 +112,58 @@ p_program = do
     p_ows
     defs <- p_def `sepBy1` p_ws
     p_ws
-    expr <- option Nothing (p_expr >>= (return . Just))
+    expr <- option (Lit 1) p_expr
     p_ows
     eof
     return (Prog defs expr)
 
+var_lookup name (n, env) = [Num $ n - frame, Num ix]
+    where
+        (frame, ix) = fromJust $ name `M.lookup` env
+
+--data Ast = Lit Int | Var String | Lam [String] Ast | Let [(String, Ast)] Ast | App [Ast] | Spec String [Ast] deriving (Show, Eq, Ord)
+
+cg n env labels (Lit x) = ([Op "LDC" [Num x]], labels)
+cg n env labels (Var name) = ([Op "LD" (name `var_lookup` (n, env))], labels)
+cg n env labels (Lam ids expr) = ([Op "LDF" [Lbl lbl]], (lbl, inner_code) : inner_labels)
+    where
+        lbl = "lambda_" ++ (show $ length labels)
+        env' = foldl (\e (ix, name) -> M.insert name (n, ix) e) env ([0 ..] `zip` ids)
+        (inner_code, inner_labels) = cg (succ n) env' labels expr
+cg n env labels (Let defs expr) = ([Op "DUM" [Num $ length defs]] ++ code' ++ [Op "LDF" [Lbl lbl], Op "RAP" [Num $ length defs]], (lbl, inner_code) : inner_labels)
+    where
+        env' = foldl (\e (ix, name) -> M.insert name (n, ix) e) env ([0 ..] `zip` (fst `map` defs))
+        (code', labels') = foldl (\(c, l) expr -> let (c', l') = cg n env' l expr in (c ++ c', l')) ([], labels) (snd `map` defs)
+        lbl = "let_" ++ (show $ length labels')
+        (inner_code, inner_labels) = cg (succ n) env' labels' expr
+cg n env labels (App (f : rest)) = (code' ++ code'' ++ [Op "AP" [Num $ length rest]], labels'')
+    where
+        (code', labels') = foldl (\(c, l) expr -> let (c', l') = cg n env l expr in (c ++ c', l')) ([], labels) rest
+        (code'', labels'') = cg n env labels' f
+cg n env labels expr = ([Cmt $ "WARNING!!! Unable to generate code for {" ++ show expr ++ "}"], labels)
+
+flatten acc [] = acc
+flatten (line, labels, acc) (op@(Op _ _) : ops) = flatten (succ line, labels, op : acc) ops
+flatten (line, labels, acc) ((Label lbl) : ops) = flatten (line, M.insert lbl line labels, (Cmt $ lbl ++ ":") : acc) ops
+flatten (line, labels, acc) (op : ops) = flatten (line, labels, op : acc) ops
+
+outarg (Num x) = show x
+outarg (Lbl lbl) = lbl
+
+out (Cmt str) = "; " ++ str
+out (Label lbl) = lbl ++ ":"
+out (Op name args) = "\t" ++ name ++ "\t" ++ (" " `intercalate` (outarg `map` args))
+
+codegen (Prog defs expr) = "\n" `intercalate` (out `map` flat_code)
+    where
+        (code, labels) = cg (-1) M.empty [] (Let defs expr)
+        (_, lbls, fc) = foldl (\acc (lbl, code) -> flatten acc ((Label lbl) : code)) (flatten (0, M.empty, []) code) (reverse labels)
+        flat_code = reverse fc
+
 main = do
     input <- getContents
     let ast = p_program `readP_to_S` input
-    mapM_ (\(x, y) -> putStrLn $ show x) ast
+    if length ast /= 1
+        then putStrLn $ "No parse or multiple parses -- " ++ show (length ast) ++ " found."
+        else putStrLn $ codegen (fst $ head ast)
 
